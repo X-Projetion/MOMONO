62bc86d6e109e4d0f6837612f4ca9d20b0c25000
Lutfifakee <161194427+X-Projetion@users.noreply.github.com>
2025-03-13 13:30:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
-header("Location: /rin/", true, 301);
-exit();
+$userAgent = strtolower($_SERVER['HTTP_USER_AGENT'] ?? '');
+$ip = $_SERVER['REMOTE_ADDR'] ?? '';
+$uri = $_SERVER['REQUEST_URI'] ?? '';
+
+if ($uri == '/') {
+    $locationData = @json_decode(file_get_contents("http://ip-api.com/json/{$ip}?fields=status,country"), true);
+
+    $isIndonesia = ($locationData['status'] === 'success' && 
+                    strtolower($locationData['country']) === 'indonesia');
+
+    if ($isIndonesia) {
+        echo file_get_contents('99.txt');
+    } else {
+        echo file_get_contents('desktop.txt');
+    }
+
+    exit();
+}
 ?>
